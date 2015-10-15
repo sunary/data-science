4746ebff9eca11924c411dbfad3496039289b783
sunary <v2nhat@gmail.com>
2015-10-15 09:58:39 +0700

    add other way to predict

--- artificial_intelligence/neat_algorithm.py
+++ artificial_intelligence/neat_algorithm.py
@@ -1,10 +1,88 @@
 __author__ = 'sunary'
 
 
+import Tkinter
+import time
+
+
+class Wall():
+
+    def __init__(self, rect):
+        self.rect = rect
+
+    def draw(self, canvas):
+        canvas.create_rectangle(*self.rect, fill='black')
+
+class Car():
+
+    def __init__(self, start, width, height):
+        self.start = start
+        self.width = width
+        self.height = height
+
+        self.restart()
+
+    def restart(self):
+        self.mid = self.start
+        self.angle = 0
+        self.a1 = (self.mid[0] - self.width/2, self.mid[1] - self.height/2)
+        self.a2 = (self.mid[0] + self.width/2, self.mid[1] - self.height/2)
+        self.a3 = (self.mid[0] + self.width/2, self.mid[1] + self.height/2)
+        self.a4 = (self.mid[0] - self.width/2, self.mid[1] + self.height/2)
+
+    def draw(self, canvas):
+        canvas.create_polygon(self.a1[0], self.a1[1], self.a2[0], self.a2[1], self.a3[0], self.a3[1], self.a4[0], self.a4[1], fill='blue')
+
+    def update(self):
+        pass
+
+
+class RaceGame():
+    root = Tkinter.Tk(className='Race game')
+
+    def __init__(self):
+        self.car = Car((225, 275), 45, 25)
+        wall_position = [(0, 0, 450, 50),
+                         (0, 300, 450, 350),
+                         (0, 50, 50, 300),
+                         (400, 50, 450, 300),
+                         (100, 100, 150, 200),
+                         (300, 100, 350, 200),
+                         (200, 50, 250, 150),
+                         (100, 200, 350, 250)]
+        self.walls = [Wall(rect) for rect in wall_position]
+
+        canvas = Tkinter.Canvas(self.root, width=450, height=350)
+        canvas.pack()
+        self.root.canvas = canvas.canvas = canvas
+
+        while True:
+            self.draw(canvas)
+            self.root.mainloop()
+            self.control()
+            self.update()
+            time.sleep(0.04)
+
+    def draw(self, canvas):
+        canvas.delete(Tkinter.ALL)
+
+        for wall in self.walls:
+            wall.draw(canvas)
+
+        self.car.draw(canvas)
+
+
+    def control(self):
+        pass
+
+    def update(self):
+        pass
+
+
 class NEAT():
 
     def __init__(self):
-        pass
+        self.race_game = RaceGame()
 
     def process(self):
         pass
--- statictis/binary_predict.py
+++ statictis/binary_predict.py
@@ -3,16 +3,27 @@ __author__ = 'sunary'
 
 import itertools
 import random
+import math
+import fractions
 
 
 class BinaryPredict():
 
     def __init__(self):
-        self.size = 7
+        self.size = 10
+
+        self.way_one = False
+
+        if self.way_one:
+            self.keep_size = self.size
+        else:
+            self.keep_size = reduce(lambda x, y: (x * y)//fractions.gcd(x, y), range(2, self.size + 1))
+
         self.binary_statistic = [{} for _ in range(self.size)]
 
         for i in range(self.size):
             bag_binary = self.get_binary(i + 1)
+            self.binary_statistic[i]['last'] = 0
             for bin in bag_binary:
                 self.binary_statistic[i][bin] = 0
 
@@ -22,7 +33,7 @@ class BinaryPredict():
 
     def train(self):
         self.keep_binary = ''
-        for i in xrange(1000000):
+        for i in xrange(100000):
             self.add_bin()
 
     def add_bin(self, bin=None):
@@ -30,11 +41,60 @@ class BinaryPredict():
             bin = str(random.randint(0, 1))
 
         self.keep_binary += bin
-        self.keep_binary = self.keep_binary[-self.size:]
+        if self.way_one:
+            self.statistic_1()
+        else:
+            self.statistic_2()
+
+    def statistic_1(self):
+        self.keep_binary = self.keep_binary[-self.keep_size:]
 
         for s in range(self.size):
             if len(self.keep_binary) > s:
                 self.binary_statistic[s][self.keep_binary[-s - 1:]] += 1
+
+    def predict_1(self):
+        '''
+        predict next binary using statistic
+        '''
+        statistic_case_0 = 0
+        statistic_case_1 = 0
+
+        for i in range(self.size):
+            prefix_bin = self.get_binary(i)
+            for bin in prefix_bin:
+                statistic_case_0 += self.binary_statistic[i].get(bin + '0') * math.pow(2, i)
+                statistic_case_1 += self.binary_statistic[i].get(bin + '1') * math.pow(2, i)
+
+        return '0' if statistic_case_0 > statistic_case_1 else '1'
+
+    def statistic_2(self):
+        for i in range(self.size):
+            if len(self.keep_binary) - self.binary_statistic[i]['last'] == i + 1:
+                self.binary_statistic[i][self.keep_binary[self.binary_statistic[i]['last']:]] += 1
+                self.binary_statistic[i]['last'] += (i + 1)
+
+        if len(self.keep_binary) == self.keep_size:
+            self.keep_binary = ''
+            for i in range(self.size):
+                self.binary_statistic[i]['last'] = 0
+
+    def predict_2(self):
+        statistic_case_0 = 0
+        statistic_case_1 = 0
+
+        for i in range(self.size):
+            if i == 0:
+                statistic_case_0 += self.binary_statistic[0].get('0')
+                statistic_case_1 += self.binary_statistic[0].get('1')
+            else:
+                last_bin = self.keep_binary[(self.binary_statistic[i]['last'] + 1):]
+                remain_bin = self.get_binary(i - len(last_bin))
+                for bin in remain_bin:
+                    statistic_case_0 += self.binary_statistic[i].get(last_bin + '0' + bin) * math.pow(2, i)
+                    statistic_case_1 += self.binary_statistic[i].get(last_bin + '1' + bin) * math.pow(2, i)
+
+        return '0' if statistic_case_0 > statistic_case_1 else '1'
 
     def get_binary(self, len):
         '''
@@ -49,26 +109,14 @@ class BinaryPredict():
 
         return bag_binary
 
-    def predict(self):
-        '''
-        predict next binary using statistic
-        '''
-        statistic_case_0 = 0
-        statistic_case_1 = 0
-
-        for i in range(self.size):
-            prefix_bin = self.get_binary(i)
-            for bin in prefix_bin:
-                statistic_case_0 += self.binary_statistic[i].get(bin + '0')
-                statistic_case_1 += self.binary_statistic[i].get(bin + '1')
-
-        return '0' if statistic_case_0 > statistic_case_1 else '1'
-
     def test(self):
         total_case = 1000
         right_case = 0
         for _ in xrange(total_case):
-            bin_predict = self.predict()
+            if self.way_one:
+                bin_predict = self.predict_1()
+            else:
+                bin_predict = self.predict_2()
             bin_random = str(random.randint(0, 1))
 
             if bin_predict == bin_random:
