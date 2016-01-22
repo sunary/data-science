3124efb51ef2fd422fba67d8bf319ab0c9e66b9d
sunary <v2nhat@gmail.com>
2016-01-22 10:37:29 +0700

    add travis yml

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ __author__ = 'sunary'
 
 import os
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
 
 def __path(filename):
@@ -14,13 +15,16 @@ if os.path.exists(__path('build.info')):
 
 version = '1.0.{}'.format(build)
 
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
+
 setup(
     name='data-science',
     version=version,
     description='Data science',
     author='Sunary',
     author_email='v2nhat@gmail.com',
+    url='https://github.com/sunary/data-science',
     packages=find_packages(exclude=['docs', 'tests*']),
-    install_requires=[
-    ]
+    install_requires=reqs
 )
