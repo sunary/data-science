4581cbb27ae05b0c2db0de296759718a8cf5f1fc
sunary <v2nhat@gmail.com>
2016-03-07 23:19:34 +0700

    using theano

---
+++ data_driven/simple.py
@@ -0,0 +1,43 @@
+__author__ = 'sunary'
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def sin_cos():
+    x = np.arange(0, 3 * np.pi, 0.1)
+    y_sin = np.sin(x)
+    y_cos = np.cos(x)
+
+    plt.plot(x, y_sin)
+    plt.plot(x, y_cos)
+
+    plt.xlabel('x axis label')
+    plt.ylabel('y axis label')
+
+    plt.title('Sine and Cosine')
+    plt.legend(['Sine', 'Cosine'])
+
+    plt.show()
+
+
+def mutil():
+    x = np.arange(0, 3 * np.pi, 0.1)
+    y_sin = np.sin(x)
+    y_cos = np.cos(x)
+
+    plt.subplot(2, 1, 1)
+    plt.plot(x, y_sin)
+    plt.title('Sine')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(x, y_cos)
+    plt.title('Cosine')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    sin_cos()
+    mutil()
